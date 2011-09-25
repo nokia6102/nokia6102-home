9b5ac13cb83f907cf7c5c1453711adfb71995f05
Leon Cheng <nokia6101@Leonteki-MacBook-Air.local>
2011-09-26 00:29:33 +0800

    Initial Commit

---
+++ 100tomarryTests/_00tomarryTests.m
@@ -0,0 +1,32 @@
+//
+//  _00tomarryTests.m
+//  100tomarryTests
+//
+//  Created by Cheng Leon on 11/9/26.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "_00tomarryTests.h"
+
+@implementation _00tomarryTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in 100tomarryTests");
+}
+
+@end
